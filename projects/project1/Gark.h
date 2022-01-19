fbdbe35657e4f0c39d02ec138bd9084ecb8f0713
abigailokonski <92695410+abigailokonski@users.noreply.github.com>
2022-01-19 10:11:24 -0800

    Create Gark.h

---
+++ projects/project1/Gark.h
@@ -0,0 +1,33 @@
+#ifndef GARK_H
+#define GARK_H
+
+//////////////////////////////////////////////////////////////////////////
+// Type definitions
+///////////////////////////////////////////////////////////////////////////
+class Mesa;  // This is needed to let the compiler know that Mesa is a
+             // type name, since it's mentioned in the Gark declaration.
+
+class Gark
+{
+public:
+
+    
+    // Constructor
+    Gark(Mesa* mp, int r, int c);
+
+    // Accessors
+    int  row() const;
+    int  col() const;
+
+    // Mutators
+    void move();
+    bool getAttacked(int dir);
+
+private:
+    Mesa* m_mesa;
+    int   m_row;
+    int   m_col;
+    int   m_health;
+};
+
+#endif
